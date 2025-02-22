a98dfb49eba905e1816422715932223c410455ab
Brendan Dash <me@aiwan.run>
2025-02-22 15:47:49 +0800

    chore: api-fortune-teller

--- src/app/ai-fortune-teller/page.tsx
+++ src/app/ai-fortune-teller/page.tsx
@@ -19,17 +19,7 @@ export default function Page() {
   );
 
   const { status, messages, setInput, handleSubmit } = useChat({
-    api: "https://api.deepseek.com/v1/chat",
-    headers: {
-      "Authorization": `Bearer sk-2d1ddf1fb7eb45e7ba408e8eb743b184`,
-      "Sec-Fetch-Mode": "cors",
-    },
-    experimental_prepareRequestBody: (options) => {
-      return {
-        ...options,
-        messages: options.messages.slice(-1),
-      };
-    },
+    api: "/api/ai-fortune-teller",
   });
 
   const message = useMemo(() => {
