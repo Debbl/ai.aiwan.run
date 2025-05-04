5f07d3e16c960f1b9de802d452c0d277b2d21c44
Brendan Dash <me@aiwan.run>
2025-05-05 01:01:58 +0800

    fix: ci

--- apps/web/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import { heroui } from "@heroui/theme";
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
-  plugins: [heroui()],
-};
-export default config;
