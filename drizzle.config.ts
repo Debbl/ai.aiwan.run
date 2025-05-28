93ce238658b5db4b0aca28d3f63532426259c224
Brendan Dash <me@aiwan.run>
2025-05-28 23:09:07 +0800

    fix: drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,19 +9,19 @@ export default defineConfig(
         out: './drizzle',
         schema: './src/server/db/schema/internal.ts',
         dialect: 'sqlite',
-        driver: 'd1-http',
         dbCredentials: {
-          accountId: CLOUDFLARE_ACCOUNT_ID,
-          databaseId: CLOUDFLARE_DATABASE_ID,
-          token: CLOUDFLARE_D1_TOKEN,
+          url: '.wrangler/state/v3/d1/miniflare-D1DatabaseObject/8288b704ef1aa1ad30abc1b56baccf178a3b0940566cc07f373946ab2a4c0e30.sqlite',
         },
       }
     : {
         out: './drizzle',
         schema: './src/server/db/schema/internal.ts',
         dialect: 'sqlite',
+        driver: 'd1-http',
         dbCredentials: {
-          url: '.wrangler/state/v3/d1/miniflare-D1DatabaseObject/8288b704ef1aa1ad30abc1b56baccf178a3b0940566cc07f373946ab2a4c0e30.sqlite',
+          accountId: CLOUDFLARE_ACCOUNT_ID,
+          databaseId: CLOUDFLARE_DATABASE_ID,
+          token: CLOUDFLARE_D1_TOKEN,
         },
       },
 )
