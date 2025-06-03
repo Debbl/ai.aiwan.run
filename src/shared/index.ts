f89187c6d24737ee32ac9cc0b7ac08f6daecf5ca
Brendan Dash <me@aiwan.run>
2025-06-04 01:02:41 +0800

    fix: get r2 url

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -5,7 +5,7 @@ export function getR2Url(key?: string) {
   if (!key) return ''
 
   const { success } = z.string().url().safeParse(key)
-  if (!success) return key
+  if (success) return key
 
   return `${R2_BASE_URL}/${key}`
 }
