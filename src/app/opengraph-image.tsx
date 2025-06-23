aed66c5198cfcf75e8feceba86b39d36762096d2
Brendan Dash <me@aiwan.run>
2025-06-23 20:07:03 +0800

    fix: force static

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import { ImageResponse } from 'next/og'
 
+export const dynamic = 'force-static'
 export const revalidate = false
 
 export const alt = 'The client first ai apps'
