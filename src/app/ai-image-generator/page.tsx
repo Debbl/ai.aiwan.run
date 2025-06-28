c8af32626d3775f9f051648f44a6cd302d09cc64
Brendan Dash <me@aiwan.run>
2025-06-28 21:07:12 +0800

    fix: cors

--- src/app/ai-image-generator/page.tsx
+++ src/app/ai-image-generator/page.tsx
@@ -51,7 +51,10 @@ export default function Page() {
   const handleDownload = async () => {
     if (!data?.generatedImageUrl) return
 
-    const res = await fetch(data.generatedImageUrl, { mode: 'cors' })
+    const res = await fetch(data.generatedImageUrl, {
+      mode: 'cors',
+      referrerPolicy: 'no-referrer',
+    })
     const blob = await res.blob()
     const url = URL.createObjectURL(blob)
     window.open(url, '_blank')
